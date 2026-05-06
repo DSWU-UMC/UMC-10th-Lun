a385a0ef28228159e520e7f9c260800f1ca9fc03
Anna0626 <lsr00626@naver.com>
2026-05-06 16:29:14 +0900

    CHORE: 5주차 Success/Error 코드 수정

--- src/main/java/com/example/umc10th/domain/review/exception/code/ReviewSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/review/exception/code/ReviewSuccessCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ReviewSuccessCode implements BaseSuccessCode{
 
-    OK(HttpStatus.OK, "USER200_1", "성공적으로 리뷰를 작성했습니다."),;
+    REVIEW_GET_SUCCESS(HttpStatus.OK, "REVIEW200_1", "성공적으로 리뷰를 조회했습니다."),
+    REVIEW_UPDATE_SUCCESS(HttpStatus.OK, "REVIEW200_2", "성공적으로 리뷰를 수정했습니다."),
+    REVIEW_DELETE_SUCCESS(HttpStatus.OK, "REVIEW200_3", "성공적으로 리뷰를 삭제했습니다."),
+    REVIEW_CREATE_SUCCESS(HttpStatus.CREATED, "REVIEW201_1", "성공적으로 리뷰를 생성했습니다."),;
 
     private final HttpStatus status;
     private final String code;
